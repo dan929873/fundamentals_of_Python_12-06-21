8d8bd352dd1c39b03cedd564b4755c27ff72247d
dan929873 <metallica04@yandex.ru>
2021-06-15 14:52:21 +0300

    hw1 v2

--- 1.py
+++ 1.py
@@ -7,10 +7,30 @@
 
 
 def sec(num):
-    print(f"{num//60} мин. {num%60} сек.")
+    year = num / 31556926
+    if (year > 1) :
+        year = num // 31556926
+        num = num - (year * 31556926)
+        print(f"{year} г. ", end='')
+    day = num / 86400
+    if (day > 1):
+        day = num // 86400
+        num = num - (day * 86400)
+        print(f"{day} д. ", end='')
+    hour = num / 3600
+    if (hour > 1):
+        hour = num // 3600
+        num = num - (hour * 3600)
+        print(f"{hour} ч. ", end='')
+    min = num / 60
+    if (min > 1):
+        min = num // 60
+        num = num - (min * 60)
+        print(f"{min} м. ", end='')
+    print(f"{num} с.")
 
 if __name__ == '__main__':
-    sec(134)
+    sec(99999999)
 
 
 
