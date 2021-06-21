51ab896dfe2c6a293ef5d169fb1076460803c90e
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-06-21 08:41:42 +0300

    update 1

--- 1.py
+++ 1.py
@@ -9,3 +9,31 @@
 # Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию, необходимую для перевода:
 # какой тип данных выбрать, в теле функции или снаружи.
 
+def num_translate(my_str):
+    """the function inpput string 'one' - 'ten' and print translate to russia number or return None if data not string 'one' - 'ten'"""
+    
+    if (my_str == 'one'):
+        print('один')
+    if (my_str == 'two'):
+        print('два')
+    if (my_str == 'three'):
+        print('три')
+    if (my_str == 'four'):
+        print('четыре')
+    if (my_str == 'five'):
+        print('пять')
+    if (my_str == 'six'):
+        print('шесть')
+    if (my_str == 'seven'):
+        print('семь')
+    if (my_str == 'eight'):
+        print('восемь')
+    if (my_str == 'nine'):
+        print('девять')
+    if (my_str == 'ten'):
+        print('десять')
+    else:
+        return None
+
+
+num_translate('one')
