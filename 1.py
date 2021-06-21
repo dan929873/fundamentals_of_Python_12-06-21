18c3b542d20d5465fc2e80727383cf1cda775762
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-06-21 14:21:26 +0300

    update 1

--- 1.py
+++ 1.py
@@ -11,27 +11,13 @@
 
 def num_translate(my_str):
     """the function inpput string 'one' - 'ten' and print translate to russia number or return None if data not string 'one' - 'ten'"""
-    
-    if (my_str == 'one'):
-        print('один')
-    if (my_str == 'two'):
-        print('два')
-    if (my_str == 'three'):
-        print('три')
-    if (my_str == 'four'):
-        print('четыре')
-    if (my_str == 'five'):
-        print('пять')
-    if (my_str == 'six'):
-        print('шесть')
-    if (my_str == 'seven'):
-        print('семь')
-    if (my_str == 'eight'):
-        print('восемь')
-    if (my_str == 'nine'):
-        print('девять')
-    if (my_str == 'ten'):
-        print('десять')
+
+    eng_num = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten'}
+    rus_num = {0: 'ноль', 1: 'один', 2: 'два', 3: 'три', 4: 'четыре', 5: 'пять', 6: 'шесть', 7: 'семь', 8: 'восемь', 9: 'девять', 10: 'десять'}
+
+    for key, val in eng_num.items():
+        if (val == my_str):
+            print(rus_num[key])
     else:
         return None
 
