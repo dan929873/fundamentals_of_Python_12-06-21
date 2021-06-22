58d4d968238fbbe37d4eb24b9c9bb20f5b7cd8bb
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-06-22 09:36:18 +0300

    update 2

--- 2.py
+++ 2.py
@@ -5,3 +5,17 @@
 # "Один"
 # >>> num_translate_adv("two")
 # "два"
+
+def thesaurus(*args):
+    my_dict = {}
+
+    for index in range(len(args)):
+        if isinstance(args[index], str):                        # проверка на строку значения подаваемого списка
+            first_word = args[index][0]
+            my_dict.setdefault(first_word, []).append(args[index])
+        else:
+            print(f"имя под номером {index + 1} не строка")
+    return my_dict
+
+
+print(thesaurus("Иван", "Мария", "Петр", "Илья", 2))
