d69428a8b77d062022825198edcebfb333db5646
kda <metallica04@yandex.ru>
2021-07-11 19:28:38 +0300

    hw7

--- 1/1.py
+++ 1/1.py
@@ -12,20 +12,30 @@
 # чтобы в будущем можно было менять имена папок под конкретный проект;
 # можно ли будет при этом расширять конфигурацию и хранить данные о вложенных
 # папках и файлах (добавлять детали)?
-
-
+import errno
 import os
+import yaml
 
 my_name_dir = {'my_project': {'settings': None, 'mainapp': None, 'adminapp': None, 'authapp': None}}
 
-def print_dir(my_dir):
-    for key in my_dir.keys():
-        print(key, '->', my_dir[key])
-        if isinstance(my_dir[key], dict):
-            print_dir(my_dir[key])
+# a_yaml_file = open(" config.yaml")
+# my_name_dir = yaml.safe_load(a_yaml_file)
 
 
-    # print(val)
+def print_dir(create_dir, my_dir = None):
+
+    if my_dir != None:
+        os.chdir(str(my_dir))
+    for key in create_dir.keys():
+        if not os.path.isdir(str(key)):
+            os.mkdir(str(key))
+        if create_dir[key] is not None:
+            print_dir(create_dir[key], key)
+
+
+
+
+
 
 print_dir(my_name_dir)
 
