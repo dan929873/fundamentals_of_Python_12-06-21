89a8066250c45bc0ebd3722bd0ef52c7327107c5
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-07-16 12:37:17 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -18,22 +18,42 @@
 # calc_cube(5: <class 'int'>)
 
 def type_logger(func):
-   cache = {}
 
    def wrapper(*args, **kwargs):
-       print('args', args)
-       print('kwargs', kwargs)
-       markup = func(*args, **kwargs)
-       print(markup)
+       # print name fun
+       print(f'{func.__name__} {args}')
+       # print type args
+       for index in range(len(args)):
+           print(f"{type(args[index])}, ", end='')
+       # print kwargs
+       if len(kwargs):
+           print("\nkwargs: ", end='')
+           for pet, name in kwargs.items():
+               print(f"{type(pet)}: {type(name)}")
+       # call fun
+       markup = func(*args)
+       print(f'\nтип значения функции: {type(markup)}')
+
        return markup
 
-   return tag_wrapper
+   return wrapper
 
 
 @type_logger
-def calc_cube(x):
-   return x ** 3
+def calc_mult(x, b):
+   return x * b
 
 
-username_f = render_input('username')
-print(render_input)
+def printScores(student, *args, **kwargs):
+   print(f"Student Name: {student}")
+   for score in args:
+       print(score)
+   for pet,name in kwargs.items():
+      print(f"{pet}: {name}")
+
+
+printScores = type_logger(printScores)
+printScores("Jonathan",100, 95, 88, 92, 99, fish=["Larry", "Curly", "Moe"])
+
+username_f = calc_mult(3, 5.6)
+print(username_f)
