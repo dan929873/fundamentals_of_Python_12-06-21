115134c5acbf9844765a5f0f99478c884815b587
dan929873 <42912179+dan929873@users.noreply.github.com>
2021-07-22 17:12:50 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,23 +6,34 @@
 
 import re
 
-
 class Date:
     def __init__(self, date) -> str:
-        reg = r'[0-3][0-9]-\d\d-\d{4}'
-        self.date = re.findall(reg, date)
-        print()
-
+        reg = r'[0-3][0-9]-[0-1][0-9]-\d{4}'
+        self.date = ''.join(map(str, re.findall(reg, date)))
 
     @classmethod
-    def data_to_num(cls):
-        pass
-        # return num
+    def data_to_num(cls, date_str):
+        dd, mm, yyyy = date_str.split('-')
+        return dd, mm, yyyy
+
 
     @staticmethod
-    def validation(data):
-        pass
+    def validation(date):
+        dd, mm, yyyy = Date.data_to_num(date)
+        d = lambda d : 0<d<32
+        if not d(int(dd)):
+            dd = None
+        m = lambda m: 0 < m < 13
+        if not m(int(mm)):
+            mm = None
+        y = lambda y: 0 < y < 2022
+        if not y(int(yyyy)):
+            yyyy = None
+        return dd, mm, yyyy
 
-ms = "23-12-2021"
+ms = "31-12-2021"
 a = Date(ms)
+
 print(a.date)
+print(Date.data_to_num(a.date))
+print(a.validation(a.date))
